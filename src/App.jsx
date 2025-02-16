657c8342bc28450517605e6f0c45c85e1b64c68a
Artiom <sinigur2006@gmail.com>
2025-02-16 18:39:26 -0500

    Module Federation: host app

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,24 @@
-import { useState } from 'react'
+import { lazy, Suspense, useState } from 'react'
 
 import Section from './Section.jsx'
 import Modal from './Modal.jsx'
 import Auth from './Auth.jsx'
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-// Import My Custom Components
-import { Button, Input } from 'vite-components'
+const Button = lazy(() => import('remoteApp/Button'));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
+    <Suspense fallback={<div>Loading...</div>}>
       <Modal trigger={<h1>Open Modal from H1</h1>}>
         Hello world
       </Modal>
 
-        <Input placeholder="Input" />
+        {/* <Input placeholder="Input" /> */}
         <Button>Button</Button>
 
       <Auth />
-
       <Section />
-    </>
+    </Suspense>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), federation({
+    name: 'hostApp',
+    remotes: {
+        remoteApp: 'http://localhost:4173/assets/remoteEntry.js',
+    },
+    shared: ['react', 'react-dom'],
+  })],
 })
